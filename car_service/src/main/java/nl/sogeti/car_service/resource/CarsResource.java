c0b59baf0bc05db04562e9001ba4e75bf6290c29
devdpm <jansen.dpm@gmail.com>
2024-11-08 23:26:44 +0100

    add resource

---
+++ car_service/src/main/java/nl/sogeti/car_service/resource/CarsResource.java
@@ -0,0 +1,54 @@
+package nl.sogeti.car_service.resource;
+
+import io.smallrye.jwt.build.Jwt;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+import nl.sogeti.car_service.di.CarRepository;
+import nl.sogeti.car_service.dto.CarDetail;
+import nl.sogeti.car_service.dto.mapper.CarMapper;
+import nl.sogeti.car_service.entity.CarEntity;
+
+import java.util.HashSet;
+import java.util.List;
+
+@Provider
+public class CarsResource implements CarsApi {
+
+    private final CarRepository carRepository;
+    private final CarMapper carMapper;
+
+//    TEMPORARY CODE FOR GENERATING JWT
+//    {
+//        System.setProperty("smallrye.jwt.sign.key.location", "privateKey.pem");
+//        String token =
+//                Jwt.issuer("http://localhost:8001/")
+//                        .upn("daniel@example.com")
+//                        .groups(new HashSet<>(List.of("Fun-User")))
+//                        .sign();
+//        System.out.println(token);
+//    }
+
+    @Inject
+    public CarsResource(CarRepository carRepository,
+                        CarMapper carMapper) {
+        this.carRepository = carRepository;
+        this.carMapper = carMapper;
+    }
+
+    @Override
+//    @RolesAllowed({ "Fun-User" })
+    public Response get(Integer id) {
+        CarEntity carEntity = getcarEntity(id);
+        CarDetail carDetail = carMapper.toCarDetails(carEntity);
+
+        return Response.ok(carDetail).build();
+    }
+
+    private CarEntity getcarEntity(Integer id) throws NotFoundException {
+        return carRepository.findByIdOptional(id.longValue())
+                                 .orElseThrow(NotFoundException::new);
+    }
+}
