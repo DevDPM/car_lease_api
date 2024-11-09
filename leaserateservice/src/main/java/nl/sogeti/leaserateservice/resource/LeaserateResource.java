6745885ecb00897c4ffc27c84c4d659c804063c6
devdpm <jansen.dpm@gmail.com>
2024-11-09 12:36:23 +0100

    add service to resource

--- leaserateservice/src/main/java/nl/sogeti/leaserateservice/resource/LeaserateResource.java
+++ leaserateservice/src/main/java/nl/sogeti/leaserateservice/resource/LeaserateResource.java
@@ -1,14 +1,26 @@
 package nl.sogeti.leaserateservice.resource;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
+import nl.sogeti.leaserateservice.dto.LeaseRateDetails;
+import nl.sogeti.leaserateservice.service.LeaserateService;
+
+import java.math.BigDecimal;
 
 @Provider
 public class LeaserateResource implements CarsApi {
 
+    private final LeaserateService leaserateService;
+
+    @Inject
+    public LeaserateResource(LeaserateService leaserateService) {
+        this.leaserateService = leaserateService;
+    }
 
     @Override
-    public Response get(Integer id, Integer mileage, Integer duration, Integer interestrate) {
-        return Response.ok().build();
+    public Response get(Integer carId, Integer mileage, Integer duration, BigDecimal interestrate) {
+        LeaseRateDetails leaseRateDetails = leaserateService.calculateLeaserate(carId, mileage, duration, interestrate);
+        return Response.ok(leaseRateDetails).build();
     }
 }
