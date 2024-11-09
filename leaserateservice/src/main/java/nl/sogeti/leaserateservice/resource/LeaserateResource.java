e079e54f1f77c59df432f5d132fc30b5058f5a6b
devdpm <jansen.dpm@gmail.com>
2024-11-09 13:33:12 +0100

    add jwt authentication

--- leaserateservice/src/main/java/nl/sogeti/leaserateservice/resource/LeaserateResource.java
+++ leaserateservice/src/main/java/nl/sogeti/leaserateservice/resource/LeaserateResource.java
@@ -1,5 +1,7 @@
 package nl.sogeti.leaserateservice.resource;
 
+import io.smallrye.jwt.build.Jwt;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
@@ -7,11 +9,24 @@ import nl.sogeti.leaserateservice.dto.LeaseRateDetails;
 import nl.sogeti.leaserateservice.service.LeaserateService;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.List;
 
 @Provider
+@RolesAllowed({"Fun-User"})
 public class LeaserateResource implements CarsApi {
 
     private final LeaserateService leaserateService;
+
+    // TEMPORARY CODE FOR GENERATING JWT FOR TESTING PURPOSE
+//    {
+//        String token =
+//                Jwt.issuer("http://localhost:9999/")
+//                        .upn("daniel@example.com")
+//                        .groups(new HashSet<>(List.of("Fun-User")))
+//                        .sign();
+//        System.out.println(token);
+//    }
 
     @Inject
     public LeaserateResource(LeaserateService leaserateService) {
@@ -20,7 +35,7 @@ public class LeaserateResource implements CarsApi {
 
     @Override
     public Response get(Integer carId, Integer mileage, Integer duration, BigDecimal interestrate) {
-        LeaseRateDetails leaseRateDetails = leaserateService.calculateLeaserate(carId, mileage, duration, interestrate);
+        LeaseRateDetails leaseRateDetails = leaserateService.getLeaserateDetails(carId, mileage, duration, interestrate);
         return Response.ok(leaseRateDetails).build();
     }
 }
