7f9182482f31ceb11f8970bf05d0e93b25d3b3ba
devdpm <jansen.dpm@gmail.com>
2024-11-09 13:35:17 +0100

    remove overhead comments

--- leaserateservice/src/main/java/nl/sogeti/leaserateservice/di/carservice/client/CarsApi.java
+++ leaserateservice/src/main/java/nl/sogeti/leaserateservice/di/carservice/client/CarsApi.java
@@ -7,57 +7,28 @@ import nl.sogeti.leaserateservice.di.carservice.exception.mapper.ApiExceptionMap
 import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-/**
- * Car service
- *
- * <p>REST Entity service about car details
- *
- */
-
 @RegisterRestClient
 @RegisterProvider(ApiExceptionMapper.class)
 @Path("/cars")
 public interface CarsApi {
 
-    /**
-     * Retrieve all cars details
-     *
-     */
     @POST
-
     @Consumes({ "application/json" })
     void create(CarDetails carDetails) throws ApiException, ProcessingException;
 
-    /**
-     * Retrieve car details
-     *
-     */
     @DELETE
     @Path("/{id}")
     void delete(@PathParam("id") Integer id) throws ApiException, ProcessingException;
 
-    /**
-     * Retrieve car details
-     *
-     */
     @GET
     @Path("/{id}")
     @Produces({ "application/json" })
     CarDetails get(@PathParam("id") Integer id) throws ApiException, ProcessingException;
 
-    /**
-     * Retrieve all cars details
-     *
-     */
     @GET
-
     @Produces({ "application/json" })
     CarDetails getAll() throws ApiException, ProcessingException;
 
-    /**
-     * Retrieve car details
-     *
-     */
     @PUT
     @Path("/{id}")
     @Consumes({ "application/json" })
